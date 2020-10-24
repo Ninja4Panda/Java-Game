f041322bc29c5dcf8028dca3aa56516278eca996
comp2511-student <z5207136@ad.unsw.edu>
2020-10-23 20:16:08 -0700

    Removed old testing code from Testing.java

--- src/test/Testing.java
+++ src/test/Testing.java
@@ -4,10 +4,6 @@ import unsw.gloriaromanus.units.Swordsman;
 
 public class Testing {
     public static void main(String[] args) {
-        Unit unitSword = new Unit(1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
-        Swordsman realSword = new Swordsman(1);
-
-        System.out.println(realSword.getClass());
         
     }
 }
