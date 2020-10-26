5c5e30f460566938a60abb663aad3784af756f3e
Solomon Chong <shinghim.chong@student.unsw.edu.au>
2020-10-25 17:41:48 -0700

    basic infrastructure for troops building

---
+++ src/unsw/gloriaromanus/Infrastructure/TroopsBuilding.java
@@ -0,0 +1,30 @@
+package unsw.gloriaromanus.Infrastructure;
+
+/**
+ * @author Solomon Chong
+ *
+ * Troops Building class to build troops
+ */
+public class TroopsBuilding extends Infrastructure {
+    private availableTroops;
+    private int baseTurns;  //Base turns needed for building to upgrade
+
+    public TroopsBuilding() {
+        super(1);
+    }
+
+    @Override
+    public Boolean upgrade() {
+        if(super.upgrade()) {
+
+        }
+    }
+
+    public trainTroops() {
+
+    }
+
+    public void getTroops() {
+
+    }
+}
