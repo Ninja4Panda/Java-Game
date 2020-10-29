f4a2fc7a6edbd1049dc697bb8f472a4e149ed596
Solomon Chong <shinghim.chong@student.unsw.edu.au>
2020-10-28 18:28:52 -0700

    Rename GameState to GamePhase, added default.json

--- src/unsw/gloriaromanus/Game.java
+++ src/unsw/gloriaromanus/Game.java
@@ -1,9 +1,12 @@
 package unsw.gloriaromanus;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
-import unsw.gloriaromanus.States.*;
+import unsw.gloriaromanus.Phase.*;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -12,49 +15,67 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Game {
+    private GamePhase preparationPhase;
+    private GamePhase attackPhase;
+    private GamePhase curPhase;
+    private GameTurn gameTurn;
     private Player curPlayer;
-    private GameState preparationPhase;
-    private GameState attackPhase;
-    private GameState curState;
-    private GameTurn turn;
     private Map<String, Player> playersMap; //Key:Faction Name, Value:Player object
 
-    public Game(String configFile) throws IOException {
+    public Game(String configFile) throws IOException, JSONException {
         //Set up the fields
         preparationPhase = new PreparationPhase(this);
         attackPhase = new AttackPhase(this);
-        curState = preparationPhase;
         playersMap = new LinkedHashMap<>();
 
         //Read state of game from config file
         String content = Files.readString(Paths.get(configFile));
-        JSONArray players = new JSONArray(content);
+        JSONObject config = new JSONObject(content);
+        JSONObject game = config.getJSONObject("Game");
 
+        //Set up current phase
+        String state = game.getString("Phase");
+        if (preparationPhase.toString().equals(state)) {
+            curPhase = preparationPhase;
+        } else if(attackPhase.toString().equals(state)) {
+            curPhase = attackPhase;
+        } else {
+            throw new JSONException("Corrupted save at State");
+        }
+
+        //Set up the players & turn
+        JSONArray players = config.getJSONArray("Players");
+        int turn = game.getInt("Turn");
+        int subTurn = game.getInt("Subturn");
+        gameTurn = new GameTurn(turn, subTurn, players.length());
+
+        curPlayer = null;
         for(int i = 0; i<players.length(); i++) {
             JSONObject playerJson = players.getJSONObject(i);
             String faction = playerJson.getString("Faction");
 
-            //Create new players and set first player as current player
+            //Create new players
             Player player = new Player(playerJson);
-            if(i==0) curPlayer = player;
+            //Set current player
+            if(subTurn == i+1) curPlayer = player;
 
             //Map each faction name to the player Object
             playersMap.put(faction, player);
         }
-        turn = new GameTurn(players.length());
+        if(curPlayer == null) throw new JSONException("Corrupted save at Subturn");
     }
 
     /**
      * @return the AttackPhase object
      */
-    public GameState getAttackPhase() {
+    public GamePhase getAttackPhase() {
         return attackPhase;
     }
 
     /**
      * @return the PreparationPhase object
      */
-    public GameState getPreparationPhase() {
+    public GamePhase getPreparationPhase() {
         return preparationPhase;
     }
 
@@ -76,8 +97,8 @@ public class Game {
      * Set the current state of the game to another state
      * @param nextState next state to be set to
      */
-    public void setCurState(GameState nextState) {
-        this.curState = nextState;
+    public void setCurPhase(GamePhase nextState) {
+        this.curPhase = nextState;
     }
 
     /**
@@ -96,14 +117,14 @@ public class Game {
             }
         }
 
-        turn.nextTurn();
+        gameTurn.nextTurn();
     }
 
     /**
      * Wrapper function to end a phase
      */
     public void endPhase() {
-        curState.endPhase();
+        curPhase.endPhase();
     }
 
     /**
@@ -111,7 +132,20 @@ public class Game {
      * See GameState for more details.
      */
     public Boolean action(String originRegion, Map<String, Integer> troops, String ... args) {
-        return curState.action(originRegion, troops, args);
+        return curPhase.action(originRegion, troops, args);
     }
 
+    public void save() throws IOException {
+        //Make the saves directory if it doesn't exists
+        File dir = new File(".","saves");
+        dir.mkdir();
+
+        //Make the new save file
+        File file = new File(dir,".json");
+        FileWriter writer = new FileWriter(file);
+
+
+        JSONObject save = curPlayer.getSave();
+        writer.write(save.toString(2));
+;    }
 }
