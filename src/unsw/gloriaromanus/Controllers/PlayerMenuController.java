412e4df672cfef01c8e2a395912f90d469a456a4
Sean Chen <z5207136@ad.unsw.edu>
2020-11-11 22:28:13 -0800

    Updated overlay

--- src/unsw/gloriaromanus/Controllers/PlayerMenuController.java
+++ src/unsw/gloriaromanus/Controllers/PlayerMenuController.java
@@ -1,7 +1,9 @@
-package unsw.gloriaromanus;
+package unsw.gloriaromanus.Controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import unsw.gloriaromanus.MenuController;
+
 
 public class PlayerMenuController extends MenuController{
     @FXML
