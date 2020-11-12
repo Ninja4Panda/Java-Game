b8a15365dd7105e47dc73cf0acd461f99347765f
Solomon Chong <shinghim.chong@student.unsw.edu.au>
2020-11-12 05:57:25 -0800

    made new scene for pause Menu

---
+++ src/unsw/gloriaromanus/Controllers/EscMenuController.java
@@ -0,0 +1,4 @@
+package unsw.gloriaromanus.Controllers;
+
+public class EscMenuController {
+}
--- src/unsw/gloriaromanus/GloriaRomanusLauncher.java
+++ src/unsw/gloriaromanus/GloriaRomanusLauncher.java
@@ -3,12 +3,6 @@ package unsw.gloriaromanus;
 public class GloriaRomanusLauncher {
 
     public static void main(String[] args) {
-        try{
-            GloriaRomanusApplication.main(args);
-
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-        
+        GloriaRomanusApplication.main(args);
     }
 }
