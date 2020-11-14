456c1f07dd89378104555c434c2fdc1dc5569283
Solomon Chong <shinghim.chong@student.unsw.edu.au>
2020-11-14 01:39:40 -0800

    fixed movementPoint bug in region

--- src/unsw/gloriaromanus/game/Game.java
+++ src/unsw/gloriaromanus/game/Game.java
@@ -200,7 +200,6 @@ public class Game implements Observer {
             Player player = it.next();
             //Set current player to next player
             if(player.equals(curPlayer)) {
-                System.out.println(curPlayer.getFaction());
                 curPlayer = it.hasNext()? it.next():firstPlayer;
                 break;
             }
