21aedc0058cfb26fae29ac8d51033aca8c80c506
Solomon Chong <shinghim.chong@student.unsw.edu.au>
2020-11-13 22:02:23 -0800

    added flag next to faction name

--- src/unsw/gloriaromanus/Controllers/PlayerMenuController.java
+++ src/unsw/gloriaromanus/Controllers/PlayerMenuController.java
@@ -2,6 +2,9 @@ package unsw.gloriaromanus.Controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import unsw.gloriaromanus.Faction.Faction;
 import unsw.gloriaromanus.MenuController;
 import unsw.gloriaromanus.Game.Player;
 
@@ -11,10 +14,15 @@ public class PlayerMenuController extends MenuController{
     private Label playerFaction;
 
     @FXML
+    private ImageView factionImage;
+
+    @FXML
     private Label playerGold;
 
     public void updatePlayer(Player player) {
-        playerFaction.setText(player.getFaction().toString());
+        Faction faction = player.getFaction();
+        factionImage.setImage(new Image(faction.getFlagPath()));
+        playerFaction.setText(faction.toString());
         playerGold.setText(Integer.toString(player.getGold()));
     }
 }
